0bcabb5c4015072ae550bff08a88b68da9044f38
lukaszFD <lukasz.f.d@gmail.com>
2019-08-18 21:19:17 +0200

    Error handling V1.0

---
+++ GlobalRepository/DB_GlobalRepository/error/Stored Procedures/AddErrorMessage.sql
@@ -0,0 +1,23 @@
+﻿create PROCEDURE error.AddErrorMessage 
+AS
+
+INSERT INTO error.ErrorMessages
+(
+	[UserName],
+	[ErrorNumber],
+	[ErrorState],
+	[ErrorSeverity],
+	[ErrorLine],
+	[ErrorProcedure],
+	[ErrorMessage]
+)
+VALUES
+(
+	SUSER_SNAME(),
+	ERROR_NUMBER(),
+	ERROR_STATE(),
+	ERROR_SEVERITY(),
+	ERROR_LINE(),
+	ERROR_PROCEDURE(),
+	ERROR_MESSAGE()
+	)
---
+++ GlobalRepository/DB_GlobalRepository/error/Tables/ErrorMessages.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [error].[ErrorMessages] (
+    [ErrorID]        INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]       VARCHAR (100)  NULL,
+    [ErrorNumber]    INT            NULL,
+    [ErrorState]     INT            NULL,
+    [ErrorSeverity]  INT            NULL,
+    [ErrorLine]      INT            NULL,
+    [ErrorProcedure] NVARCHAR (MAX) NULL,
+    [ErrorMessage]   NVARCHAR (MAX) NULL,
+    [ErrorDateTime]  DATETIME       DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([ErrorID] ASC)
+);
+
