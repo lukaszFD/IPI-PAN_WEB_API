7561bb45eaacb0a0c56d36eca9b53d140ff1f931
Łukasz <lukasz.f.d@gmail.com>
2019-10-12 14:43:17 +0200

    new schema

---
+++ GlobalRepository/DB_GlobalRepository/Security/service.sql
@@ -0,0 +1,3 @@
+﻿CREATE SCHEMA [service]
+    AUTHORIZATION [dbo];
+
---
+++ GlobalRepository/DB_GlobalRepository/service/Stored Procedures/EnableAllConstraints.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE PROCEDURE service.EnableAllConstraints
+AS
+BEGIN TRY
+	EXEC sp_MSforeachtable @command1="print '?'", @command2="ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all"
+END TRY
+BEGIN CATCH
+	EXECUTE [GlobalRepository].[error].[AddErrorMessage] 
+			@schemaName = 'service',
+			@tableName = null, 
+			@columnName = null,
+			@columnId = null 
+END CATCH
