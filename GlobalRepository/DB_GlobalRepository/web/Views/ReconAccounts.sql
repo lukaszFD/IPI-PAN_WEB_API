b42424fa6eba6dadc6169528c23aa6fdfbef2f5b
Łukasz <lukasz.f.d@gmail.com>
2019-11-11 11:40:55 +0100

    new renocn views for web
    Co-Authored-By: Łukasz <lukasz.f.d@gmail.com>

---
+++ GlobalRepository/DB_GlobalRepository/web/Views/ReconAccounts.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW web.ReconAccounts
+AS
+
+SELECT
+	 [AccountExId]
+    ,[CountryRegionCode]
+    ,[UserExId]
+    ,[SystemExId]
+    ,[ServerExId]
+    ,[Name]
+    ,[Description]
+    ,[Type]
+    ,[PasswordExpires]
+FROM 
+	[recon].[Accounts]
