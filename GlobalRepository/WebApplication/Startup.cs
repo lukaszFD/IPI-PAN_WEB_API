faa4af8929726af11825bcd30e485ae1b10c0e37
Łukasz <lukasz.f.d@gmail.com>
2019-11-23 16:37:43 +0100

    WebApplication

---
+++ GlobalRepository/WebApplication/Startup.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace WebApplication
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(a => a.SwaggerDoc("v1", new Info { Title = "Swagger Library", Description = "Swagger WebAPI Library" }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                a =>
+                {
+                    a.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Library");
+                }
+                );
+        }
+    }
+}
